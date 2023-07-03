c95fe37ae372759615e9551d22d56f4f89f3dc5e
Parkjju <rudwns3927@gmail.com>
2023-07-04 08:35:28 +0900

    feat: 키체인 로직 추가

--- keychain-tutorial/ViewController.swift
+++ keychain-tutorial/ViewController.swift
@@ -40,7 +40,36 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: ASAuthorizationControllerDelegate{
-
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            // Create an account in your system.
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            // For the purpose of this demo app, store the `userIdentifier` in the keychain.
+            self.saveUserInKeychain(userIdentifier)
+            
+            // For the purpose of this demo app, show the Apple ID credential information in the `ResultViewController`.
+            self.showResultViewController(userIdentifier: userIdentifier, fullName: fullName, email: email)
+        
+        case let passwordCredential as ASPasswordCredential:
+        
+            // Sign in using an existing iCloud Keychain credential.
+            let username = passwordCredential.user
+            let password = passwordCredential.password
+            
+            // For the purpose of this demo app, show the password credential as an alert.
+            DispatchQueue.main.async {
+                self.showPasswordCredentialAlert(username: username, password: password)
+            }
+            
+        default:
+            break
+        }
+    }
 }
 
 extension ViewController: ASAuthorizationControllerPresentationContextProviding{
